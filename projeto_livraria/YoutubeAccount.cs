29db6e6dcf6aeec05e9b8e85495c0a0b8f4f6f4c
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-08 17:01:04 +0100

    Class implements IYoutubeAPIAccount and inherits from SyncAccount

---
+++ projeto_livraria/YoutubeAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_livraria
+{
+    public class YoutubeAccount : SyncAccount ,  IYoutubeAPIAccount
+    {
+        public string email => throw new NotImplementedException();
+
+        public string password { set => throw new NotImplementedException(); }
+
+        public void changeAcccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void searchAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void syncronize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
