f6e0968baea485f4537c1c19ac71fd2530844c19
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-08 15:59:03 +0100

    Interface Created: for Youtube API related to account

---
+++ projeto_livraria/IYoutubeAPIAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_livraria
+{
+    interface IYoutubeAPIAccount
+    {
+
+        string email { get; }
+        string password { set; }
+
+        public void changeAcccount();
+
+        public void syncronize();
+
+        public void searchAccount();
+
+    }
+}
