de015ccd2b764d6fd66b132249dc379ca79cebd8
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-08 16:13:51 +0100

    Class created: main variables and methods created with out importante content

---
+++ projeto_livraria/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_livraria
+{
+    public class User
+    {
+
+        public string userName { get; set; }
+        
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public string biography { get; set; }
+
+        public string youtubeAccount { get; set; }
+
+        //public PlanPayment plan { get; set; }
+        public int maxPlaylist { get; set; }   
+
+    
+        public void changeBiography( )
+        {
+            Console.WriteLine( "Changing bio" );
+        } 
+        public void changePassword( )
+        {
+            Console.WriteLine( "Changing bio" );
+        }
+        public void updatePlan( )
+        {
+            Console.WriteLine( "Updating plan" );
+        }
+        public void canclePlan( )
+        {
+            Console.WriteLine( "Canceling plan" );
+        }
+
+
+        
+    
+    }
+}
