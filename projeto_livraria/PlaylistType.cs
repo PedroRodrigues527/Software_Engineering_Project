dc708254c9239775e9e48783267b2183c5be999d
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-08 15:36:35 +0100

    Class Created: Enumeration for type of playlist

---
+++ projeto_livraria/PlaylistType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_livraria
+{
+    enum PlaylistType
+    {
+        PUBLIC,
+        PRIVATE
+    }
+}
