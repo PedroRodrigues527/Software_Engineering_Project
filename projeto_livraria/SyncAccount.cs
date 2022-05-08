be13469aba3c88ba82c2cb1ef496c5fbb18aa7c3
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-08 16:59:12 +0100

    Created Class

---
+++ projeto_livraria/SyncAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_livraria
+{
+    public class SyncAccount
+    {
+        private string email { get; set; }
+        private string password { get; set; }
+
+        public void changePassword()
+        {
+            Console.WriteLine( "Changing password from SyncAccount" );
+        }
+
+        public void syncronize()
+        {
+            Console.WriteLine( "Syncronize account from SyncAccount" );
+        }
+    }
+}
