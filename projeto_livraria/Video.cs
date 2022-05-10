68e38b81b87c9edc0b4f22eabb90868ac0157e4c
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-10 09:23:38 +0100

    Create class Video.cs

---
+++ projeto_livraria/Video.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_livraria
+{
+    public class Video : IYoutubeAPIVideos
+    {
+
+        public string artist { get; set; }
+        public string dataCreating { get; set; }
+        public string url { get; set; }
+        public string title => throw new NotImplementedException();
+
+        public string searchPlaylist(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string searchVideo(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
