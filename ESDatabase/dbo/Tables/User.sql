fb84a139c4a20ddd0c5cc1902eb3604f6b6ed302
JoaoCupido <jcupido13@gmail.com>
2022-05-11 23:39:16 +0100

    Base de dados Implementada; só falta fazer Publish... da base de dados ESDatabase.sqlproj para a base de dados do Visual Studio. FONTE: https://www.c-sharpcorner.com/article/how-to-create-sql-server-database-project-with-visual-studio/

---
+++ ESDatabase/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User] (
+    [ID]       INT           IDENTITY (1, 1) NOT NULL,
+    [Username] NCHAR (16)    COLLATE Latin1_General_CS_AS NULL,
+    [Email]    NVARCHAR (50) COLLATE Latin1_General_CS_AS NULL,
+    [Password] NCHAR (16)    COLLATE Latin1_General_CS_AS NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [UC_Person] UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
