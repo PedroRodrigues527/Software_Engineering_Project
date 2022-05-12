bfc148bb4d12aefd7b71db33e025629996324daf
JoaoCupido <jcupido13@gmail.com>
2022-05-12 13:08:26 +0100

    feito a inserção de fontawesome icons + validação corretamente feita com header dynamic (loggedaccount or not)

--- Projeto_ES/Program.cs
+++ Projeto_ES/Program.cs
@@ -12,7 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazoredSessionStorage();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddTransient<IUserData, UserData>();
 
