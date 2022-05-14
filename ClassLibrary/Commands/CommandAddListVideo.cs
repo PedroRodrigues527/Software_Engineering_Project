584f3b8d703d6c4ec6ea3e2b2b08144dff5baac4
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-14 15:31:35 +0100

    Command Pattern  Skeleton implemented

---
+++ ClassLibrary/Commands/CommandAddListVideo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Commands
+{
+    public class CommandAddListVideo : ICommand
+    {
+        public void Execute()
+        {
+            //Add video to playlist
+            throw new NotImplementedException();
+        }
+
+        public void Redo()
+        {
+            //Add Video again from playlist
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            //Remove video from playlist
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClassLibrary/Commands/CommandRemoveListVideo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Commands
+{
+    public class CommandRemoveListVideo : ICommand
+    {
+        public void Execute()
+        {
+            //Remove Video
+            throw new NotImplementedException();
+        }
+
+        public void Redo()
+        {
+            //Remove again from playlist
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            //Add video to playlist
+            throw new NotImplementedException();
+        }
+    }
+}
