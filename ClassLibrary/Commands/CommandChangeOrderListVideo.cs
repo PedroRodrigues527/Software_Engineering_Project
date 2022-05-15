fc2aa629526a16541bdd41c3365008be0bf767e4
JoaoCupido <jcupido13@gmail.com>
2022-05-15 23:20:41 +0100

    initialize partial command swap order

---
+++ ClassLibrary/Commands/CommandChangeOrderListVideo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Commands
+{
+    public class CommandChangeOrderListVideo : ICommand
+    {
+        public List<Video> ListVideos { get; }
+        public List<Video> OldListVideos { get; }
+        public Playlist PlaylistEdit;
+        public IPlaylistData PlaylistData;
+        
+        public CommandChangeOrderListVideo(List<Video> listVideos, List<Video> oldListVideos, IPlaylistData playlistData, Playlist playlistEdit)
+        {
+            ListVideos = listVideos;
+            PlaylistData = playlistData;
+            PlaylistEdit = playlistEdit;
+            OldListVideos = oldListVideos;
+        }
+
+        public void Execute()
+        {
+            
+            for (int i = 0; i < ListVideos.Count; i++)
+            {
+                ListVideos[i].Order = i;
+            }
+        }
+
+        public void Redo()
+        {
+            Execute();
+        }
+
+        public void Undo()
+        {
+            //Set order before swap
+        }
+    }
+}
