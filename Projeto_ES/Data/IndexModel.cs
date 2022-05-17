c61c67813d694e51b087ed54bcdae3013582e212
JoaoCupido <jcupido13@gmail.com>
2022-05-17 14:50:50 +0100

    Possibilidade de adicionar vídeos de uma playlist com certas restrições na playlist do nosso website

--- Projeto_ES/Data/IndexModel.cs
+++ Projeto_ES/Data/IndexModel.cs
@@ -16,7 +16,7 @@ namespace Projeto_ES.Data
             this.youTubeService = youTubeService;
         }
 
-        public List<Video> OnGet(string videoId)
+        public List<Video> OnGetVideo(string videoId)
         {
             var searchListRequest = youTubeService.Videos.List("snippet");
             searchListRequest.Id = videoId;
@@ -30,6 +30,25 @@ namespace Projeto_ES.Data
                 Title = video.Snippet.Title,
                 ChannelName = video.Snippet.ChannelTitle,
                 VideoId = video.Id,
+            }));
+
+            return Videos;
+        }
+
+        public List<Video> OnGetVideosPlaylist(string playlistId)
+        {
+            var searchListRequest = youTubeService.PlaylistItems.List("snippet");
+            searchListRequest.PlaylistId = playlistId;
+            searchListRequest.MaxResults = 100;
+
+            var searchListResponse = searchListRequest.Execute();
+            List<Video> Videos = new List<Video>();
+            Videos.AddRange(searchListResponse.Items.Select(video => new Video
+            {
+                Thumbnail = video.Snippet.Thumbnails.Default__.Url,
+                Title = video.Snippet.Title,
+                ChannelName = video.Snippet.VideoOwnerChannelTitle,
+                VideoId = video.Snippet.ResourceId.VideoId,
             }));
 
             return Videos;
